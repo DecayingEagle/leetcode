26c1b9cedd0bb9d854844d2ad8a7dfcc4fa0e061
DecayingEagle <46646694+DecayingEagle@users.noreply.github.com>
2023-05-14 21:39:52 +0200

    #1 solved, slow. universal tester not functional

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Program {
+	static public void Main(string[] args) {
+		int[] arr = {3,2,4};
+        int[] print = TwoSum(arr, 6);
+		System.Console.WriteLine(print[0] + "," + print[1]);
+	}
+	static public int[] TwoSum(int[] nums, int target) {
+		int sum = 0;
+		int[] returnValue = {-1, -1};
+		for (int i = 0; i < nums.Length; i++)
+		{
+			for (int j = 1; j < nums.Length; j++)
+			{
+				sum = nums[i] + nums[j];
+				System.Console.WriteLine(sum.ToString());
+				System.Console.WriteLine(j.ToString());
+				System.Console.WriteLine(i.ToString());
+				if ((sum == target)&&(i != j)) { returnValue[1] = j; returnValue[0] = i; break; }
+			}
+			if (sum == target) { break; }
+		}
+		return returnValue;
+    }
+    
+}
---
+++ template/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+namespace Template {
+    public class TestCase {
+    public TestCase()
+    {
+        int Id = -1;
+        String[] input;
+        String[] output;
+    }
+
+    public int Id { get; set; }
+    public String[] input { get; set; }
+    public String[] output { get; set; }
+}
+public class Program
+{
+    public static void Main() {
+        string fileName = "testcases";
+        string jsonString = File.ReadAllText(fileName);
+        
+    }
+}
+}
+
+
