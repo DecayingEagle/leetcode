645c6b236e64b6203ef1995693980eb5c13c0082
DecayingEagle <46646694+DecayingEagle@users.noreply.github.com>
2023-05-21 23:23:44 +0200

    3

---
+++ 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Program {
+	
+	public class ListNode {
+		public int val;
+		public ListNode next;
+		public ListNode(int val=0, ListNode next=null) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	static public void Main(string[] args) {
+		ListNode twoNums1 = new ListNode();
+		ListNode twoNums2 = new ListNode();
+	}
+
+    static public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        
+    }
+}
---
+++ 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public class Program {
+	static public void Main(string[] args) {
+		String s1 = "abcabcbb";
+		String s2 = "bbbbb";
+		String s3 = "pwwkew";
+        System.Console.WriteLine(LengthOfLongestSubstring(s1));
+        System.Console.WriteLine(LengthOfLongestSubstring(s2));
+        System.Console.WriteLine(LengthOfLongestSubstring(s3));
+	}
+	static public int LengthOfLongestSubstring(string s) {
+        char[] sArr = s.ToCharArray(0, s.Length);
+        String lastChrs = "";
+        int longest = -1;
+        for (int i = 0; i < sArr.Length; i++)
+        { 
+            if(lastChrs.EndsWith(sArr[i])) {
+                char[] buffer = lastChrs.ToCharArray;
+                //!bro fix later
+                longest = lastChrs.Length;
+                lastChrs = sArr[i].ToString();
+            } else {
+                lastChrs = String.Concat(lastChrs, sArr[i].ToString());
+            }
+
+            System.Console.WriteLine("lstChrs: " + lastChrs);
+            System.Console.WriteLine("sArr[i]: " + sArr[i].ToString());
+        }
+        return longest;
+    }
+    
+}
