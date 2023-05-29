1e9a0c993bfaacb879d8de8738bb5600ff6ddd64
Marcin Rozalski <therealeagle.yt@gmail.com>
2023-05-29 03:35:29 +0200

    49 needs fixing, I need to return correct strings and test code
    I need to either change the detection algorithm or change to unordered_map, use this quote for reference:
    i also totally forgot that i am not doing last one where i could just check if they are anagrams,
    unless i change my anagram checking algorithm i would need to use 2 unordered maps instead of this or one acting like a dictionary unless c++ has dictionaries although i doubt dictionaries are as fast as unordered maps

--- 49/main.cpp
+++ 49/main.cpp
@@ -9,20 +9,55 @@
  * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
  *
  * Constraints:
- * 1 <= strs.length <= 104
+ * 1 <= strs.length <= 10^4
  * 0 <= strs[i].length <= 100
  * strs[i] consists of lowercase English letters.
  */
+#include <algorithm>
 #include <iostream>
+#include <string>
 #include <vector>
 
 class Solution49 {
 public:
     std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
-        return std::vector<std::vector<std::string>>();
+        std::vector<std::vector<std::string>> ret;
+        if (strs.size() == 1) {
+            ret.push_back(std::vector({strs[0]}));
+            return ret;
+        } else if (strs.size() == 0) {
+            return ret;
+        }
+        for(int i; i <= strs.size(); i++) {
+            std::sort(strs[i].begin(), strs[i].end());
+        }
+        std::sort(strs.begin(), strs.end());
+        std::vector<std::string> buffer;
+        for (int i = 1; i <= strs.size(); i++) {
+            buffer.push_back(strs[i-1]);
+            if (strs[i-1] != strs[i]) {
+                ret.push_back(buffer);
+                buffer.clear();
+            }
+        }
+        buffer.push_back(strs[strs.size()-1]);
+        ret.push_back(buffer);
+        return ret;
     }
 };
 
 int main() {
-
+    Solution49 s;
+    std::vector<std::string> str = {"eat","tea","tan","ate","nat","bat"};
+    std::vector<std::vector<std::string>> res = s.groupAnagrams(str);
+    //std::cout << "[";
+    for (int i; i <= res.size(); i++) {
+        std::cout << "----------------------------" << std::endl;
+        //std::cout << "[";
+        for (int j; j <= res[i].size(); j++) {
+            std::cout << res[j][i] << std::endl;
+            //std::cout << res[j][i] << (j<=res[i].size() ? "," : "]");
+        }
+        //std::cout << (i<=res.size() ? "]," : "]");
+    }
 }
