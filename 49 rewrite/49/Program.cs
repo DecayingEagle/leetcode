c865aac88b6c5a11522c50386c073b73ad684184
Marcin Rozalski <therealeagle.yt@gmail.com>
2023-06-24 19:08:17 +0200

    24.06 just idk lol

--- 49 rewrite/49/Program.cs
+++ 49 rewrite/49/Program.cs
@@ -1,55 +1,103 @@
-﻿using System;
+﻿/**
+ *  49. Group Anagrams
+ *
+ * Given an array of strings strs, group the anagrams together. You can return 
+ * the answer in any order.
+ * An Anagram is a word or phrase formed by rearranging the letters of a 
+ * different word or phrase,
+ * typically using all the original letters exactly once.
+ *
+ * Example 1:
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ *
+ * Constraints:
+ * 1 <= strs.length <= 10^4
+ * 0 <= strs[i].length <= 100
+ * strs[i] consists of lowercase English letters.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Collections;
 using System.ComponentModel;
-public class Program {
-    static public int Main(string[] args) {
-        return 0;
-    }
-    public List<List<string>> GroupAnagrams(string[] strs) {
-        static string SortCharAlphabetically(string s) {
-            char[] cBuffer = s.ToArray();
-            Array.Sort(cBuffer);
-            return new string(cBuffer);
+using System.Globalization;
+
+namespace _49
+{
+    public class Program
+    {
+        static public int Main()
+        {
+            string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+            IList<IList<string>> ret = GroupAnagrams(strs);
+
+            Console.WriteLine(ret.ToString());
+            return 0;
         }
-        List<List<string>> ret = new List<List<string>>();
-        List<string> listBuffer = new List<string>();
-        if (strs.Length == 1) {
-            listBuffer.Add(new String(strs[0]));
-            ret.Add(listBuffer);
-            return ret;
-        } else if (strs.Length == 0) {
-            return ret;
-        }
-
-        Array.Sort(strs);
-
-        /* 
-         * data structure of my shit should be something like this:
-         * we've got 2 main data sets: the original strings, and the list of
-         * string character sorted for referencing it to make later an array
-         * the original strings should have a value assigned to them to connect
-         * them to the iterator but since HashSet is unordered I need to store
-         * the original string to find later to iterate through later to find
-         * a group
-         */
-
-        //dictStrs = <ogStr, sortedStr
-        Dictionary<string, string> dictStrs = new Dictionary<string, string>();
-        HashSet<string> setSortedStrs = new HashSet<string>();
-        string sTmp;
-        int id = 0;
-        foreach(string s in strs) {
-            id++;
-            SortCharAlphabetically(s);
-            if (setSortedStrs.TryGetValue(strs[id],out sTmp)) {
-                dictStrs.Add(strs[id], sTmp);
-                continue;
-            } else {
-                setSortedStrs.Add(s);
-                dictStrs.Add(strs[id], sTmp);
+        static public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            static string SortCharAlphabetically(string s)
+            {
+                char[] cBuffer = s.ToArray();
+                Array.Sort(cBuffer);
+                return new string(cBuffer);
             }
+            IList<IList<string>> ret = new List<IList<string>>();
+            IList<string> listBuffer = new List<string>();
+            if (strs.Length == 1)
+            {
+                listBuffer.Add(strs[0]);
+                ret.Add(listBuffer);
+                return ret;
+            }
+            else if (strs.Length == 0)
+            {
+                return ret;
+            }
+
+            Array.Sort(strs);
+
+            /** 
+             * data structure of my shit should be something like this:
+             * we've got 2 main data sets: the original strings, and the list of
+             * string character sorted for referencing it to make later an array
+             * the original strings should have a value assigned to them to connect
+             * them to the iterator but since HashSet is unordered I need to store
+             * the original string to find later to iterate through later to find
+             * a group
+             */
+
+            // * dictStrs = <ogStr, sortedStr>
+
+            Dictionary<string, string> dictStrs = new();
+            IList<string> listSortedStrs = new List<string>();
+            int id = 0;
+            string sortedStr, s_tmp;
+            foreach (string s in strs)
+            {
+                sortedStr = SortCharAlphabetically(s);
+                if (!listSortedStrs.Contains(sortedStr))
+                {
+                    listSortedStrs.Add(sortedStr);
+                }
+                dictStrs.Add(id.ToString()+strs[id], sortedStr);
+                id++;
+            }
+
+            foreach (string sorted in listSortedStrs)
+            {
+                listBuffer = new List<string>();
+                foreach (var pair in dictStrs)
+                {
+                    if (pair.Value.EndsWith(sorted))
+                    {
+                        listBuffer.Add(pair.Key);
+                    }
+                }
+                ret.Add(listBuffer);
+            }
+            return ret;
         }
-        return ret;
     }
 }
